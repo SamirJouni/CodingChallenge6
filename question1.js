1a4da6ab9c806df2133ac35c95b0b8965f9c8153
F1r3byte <f1r3byte@yandex.com>
2018-06-15 23:02:41 +0200

    added some more code to question1

--- question1.js
+++ question1.js
@@ -1,6 +1,17 @@
 function cleanTheRoom (array) {
+	array.sort();
+	var length = array.length - 1;
 	var cleanedArray = [];
 
 
+	for (var i = 0; i < length; i++){
+		if (array[i] === array[i+1]) {
+
+		}
+		else {
+
+		}
+
+	}
 	return cleanedArray;
 }
