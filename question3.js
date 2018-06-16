5bfcf255b6da6f48bb8bd5040d544024c1a2d0a0
F1r3byte <f1r3byte@yandex.com>
2018-06-16 23:54:50 +0200

    added calls to functions

--- question3.js
+++ question3.js
@@ -1,6 +1,16 @@
 function convertColor(color) {
 	var convertedColor;
 
+	if ( color[0] === '#' ) {
+
+	convertedColor	= convertHexToRgb(color.slice(1));
+	}
+	else if ( color[0] === '(' ) {
+		convertedColor = convertRgbToHex(color);
+	}
+	else{
+		return "Error ! \nPlease Enter A Proper Input. \nHex starts with an octothorpe and has 6 trailing numbers, rgb is enclsed in paranthesis and seperated with commas. "
+	}
 	function convertHexToRgb(hexNumber) {
 		var decimalNumber = parseInt(hexNumber, 16);
 		var r = (decimalNumber >> 16) & 255;
@@ -18,5 +28,6 @@ function convertColor(color) {
 		var numberInHex = number.toString(16);
 		return numberInHex.length == 1 ? "0" + numberInHex : numberInHex;
 	}
+
 	return convertedColor;
 }
