69bf86b68c77d6a69dc719a754ba4479b5045c6a
F1r3byte <f1r3byte@yandex.com>
2018-06-16 22:03:49 +0000

    fixed input for functions

--- question3.js
+++ question3.js
@@ -1,7 +1,18 @@
 function convertColor(color) {
 	var convertedColor;
 
-	function convertHexToRgb(hexNumber) {
+	if ( color[0] === '#' ) {
+
+	convertedColor	= convertHexToRgb(color);
+	}
+	else if ( color[0] === '(' ) {
+		convertedColor = convertRgbToHex(color);
+	}
+	else{
+		return "Error ! \nPlease Enter A Proper Input. \nHex starts with an octothorpe and has 6 trailing numbers, rgb is enclsed in paranthesis and seperated with commas. "
+	}
+	function convertHexToRgb(color) {
+		var hexNumber = color.slice(1);
 		var decimalNumber = parseInt(hexNumber, 16);
 		var r = (decimalNumber >> 16) & 255;
 		var g = (decimalNumber >> 8) & 255;
@@ -10,13 +21,19 @@ function convertColor(color) {
 		return "(" + r + "," + g + "," + b + ")";
 	}
 
-	function convertRgbToHex(r, g, b) {
-		return "#" + numberToHex(r) + numberToHex(g) + numberToHex(b);
+	function convertRgbToHex(color) {
+		color = color.slice(1,-1).split(',');
+		var r = color[0];
+		var g = color[1];
+		var b = color[2];
+		var a = color[3] ? color[3] : '';
+		return "#" + numberToHex(r) + numberToHex(g) + numberToHex(b) + numberToHex(a);
 	}
 
 	function numberToHex(number) {
 		var numberInHex = number.toString(16);
 		return numberInHex.length == 1 ? "0" + numberInHex : numberInHex;
 	}
+
 	return convertedColor;
 }
